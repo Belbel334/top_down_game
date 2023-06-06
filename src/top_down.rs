c1a7a6e3c5dc68cf592f42c66fe9926d7ef1af14
Timo Dehandschutter <timo.dehandschutter@gmail.com>
2023-06-06 09:51:33 +0200

    started working on menu struct

--- src/top_down.rs
+++ src/top_down.rs
@@ -3,4 +3,5 @@ pub mod tile_map;
 pub mod camera;
 pub mod animation;
 pub mod enemy;
+pub mod menu;
 mod path_finder;
---
+++ src/top_down/menu.rs
@@ -0,0 +1,31 @@
+use sdl2::pixels::Color;
+use sdl2::video::Window;
+use sdl2::rect::Rect;
+use sdl2::render::{Texture, Canvas};
+
+pub struct Menu<'a>
+{
+    texture: &'a Texture<'a>,
+    play_src: Rect,
+    play_dst: Rect,
+    logo_src: Rect,
+    logo_dst: Rect,
+    bg_color: Color,
+}
+
+impl Menu<'_>
+{
+    pub fn new<'a>( texture: &'a Texture<'a>, play_src: Rect, play_dst: Rect, logo_src: Rect, logo_dst: Rect, bg_color: Color ) -> Menu<'a>
+    {
+        Menu { texture, play_src, play_dst, logo_src, logo_dst, bg_color }
+    }
+
+    pub fn draw( &self, canvas: &mut Canvas<Window> ) -> Result<(), String>
+    {
+        canvas.set_draw_color(self.bg_color);
+        canvas.clear();
+        canvas.copy(self.texture, self.play_src, self.play_dst)?;
+        canvas.copy(self.texture, self.logo_src, self.logo_dst)?;
+        Ok(())
+    }
+}
